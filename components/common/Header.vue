49db5cea9c4590dda15634323de6bdd87baa8783
Naoki <nshiji@smartshopping.co.jp>
2020-04-04 07:09:15 +0900

    ヘッダー、フッター作成

---
+++ components/common/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <i class="material-icons menu-icon">menu</i>
+    <div class="search-bar">
+      <i class="material-icons search-icon">search</i>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Header'
+})
+</script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  background-color: $color-black;
+  padding: 7px 12px;
+  width: 100%;
+
+  .menu-icon {
+    font-size: 34px;
+    color: white;
+  }
+
+  .search-bar {
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+    padding-left: 10px;
+    width: 100%;
+    background-color: white;
+    border-radius: 20px;
+
+    .search-icon {
+      font-size: 30px;
+    }
+  }
+}
+</style>
